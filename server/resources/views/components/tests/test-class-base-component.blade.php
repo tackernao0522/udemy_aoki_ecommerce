933d0f946bcf2083eaecdbb2cd165a7fdd7c8dd8
takaki <cheap_trick_magic@yahoo.co.jp>
2021-09-21 18:09:19 +0900

    ClassBase

---
+++ server/resources/views/components/tests/test-class-base-component.blade.php
@@ -0,0 +1,9 @@
+<div>
+    クラスベースのコンポーネントです。
+    <div>{{ $classBaseMessage }}</div>
+    <div>{{ $defaultMessage }}</div>
+    <!-- Walk as if you are kissing the Earth with your feet. - Thich Nhat Hanh -->
+</div>
+
+<!-- php artisan make:component TestClassBase -->
+<!-- php artisan view:clear -->
