611cd99bb1414f89feb3c32d42c039bc2d9f43e7
takaki <cheap_trick_magic@yahoo.co.jp>
2021-09-26 18:14:11 +0900

    管理者オーナー一覧画面をtailblocksを利用してレイアウト調整

--- server/app/Http/Controllers/Admin/OwnersController.php
+++ server/app/Http/Controllers/Admin/OwnersController.php
@@ -17,13 +17,13 @@ class OwnersController extends Controller
 
     public function index()
     {
-        $date_now = Carbon::now();
-        $date_parse = Carbon::parse(now());
-        echo $date_now->year;
-        echo $date_parse;
+        // $date_now = Carbon::now();
+        // $date_parse = Carbon::parse(now());
+        // echo $date_now->year;
+        // echo $date_parse;
 
-        $e_all = Owner::all();
-        $q_get = DB::table('owners')->select('name', 'created_at')->get();
+        // $e_all = Owner::all(); // eloquent
+        // $q_get = DB::table('owners')->select('name', 'created_at')->get(); // QueryBuilder
         // $q_first = DB::table('owners')->select('name')->first();
 
         // $c_test = collect([
@@ -34,7 +34,9 @@ class OwnersController extends Controller
 
         // dd($e_all, $q_get, $q_first, $c_test);
 
-        return view('admin.owners.index', compact('e_all', 'q_get'));
+        $owners = Owner::select('name', 'email', 'created_at')->get();
+
+        return view('admin.owners.index', compact('owners'));
     }
 
     /**
