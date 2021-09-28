2dc63fb3aed8855c500116673a0f27c21b2e7ffb
takaki <cheap_trick_magic@yahoo.co.jp>
2021-09-28 17:19:54 +0900

    Shopリレーション1対1の設定

--- server/app/Models/Shop.php
+++ server/app/Models/Shop.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Owner;
 
 class Shop extends Model
 {
     use HasFactory;
+
+    public function owner()
+    {
+        return $this->belongsTo(Owner::class);
+    }
 }
+
+// php artisan tinker
+// $owner1 = App\Models\Owner::fint(1)->shop;
+// $owner1 = App\Models\Owner::fint(1)->shop->name;
+// App\Models\Shop::find(1)->owner
+// App\Models\Shop::find(1)->owner->name
