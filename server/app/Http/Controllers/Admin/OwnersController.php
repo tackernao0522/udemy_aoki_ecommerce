6a0c28b0c4bcb7e41bbc7e54f837b13ebbced7b4
takaki <cheap_trick_magic@yahoo.co.jp>
2021-09-29 16:51:46 +0900

    Shopの作成(トランザクション/例外/エラー処理)

--- server/app/Http/Controllers/Admin/OwnersController.php
+++ server/app/Http/Controllers/Admin/OwnersController.php
@@ -9,6 +9,9 @@ use App\Models\Owner; // Eloquent
 use Illuminate\Support\Facades\DB; // QueryBuilder
 use Carbon\Carbon;
 use Illuminate\Validation\Rules;
+use App\Models\Shop;
+use Throwable;
+use Illuminate\Support\Facades\Log;
 
 class OwnersController extends Controller
 {
@@ -65,11 +68,26 @@ class OwnersController extends Controller
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
-        Owner::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
+        try {
+            DB::transaction(function () use ($request) {
+                $owner = Owner::create([
+                    'name' => $request->name,
+                    'email' => $request->email,
+                    'password' => Hash::make($request->password),
+                ]);
+
+                Shop::create([
+                    'owner_id' => $owner->id,
+                    'name' => '店名を入力してください',
+                    'information' => '',
+                    'filename' => '',
+                    'is_selling' => true,
+                ]);
+            }, 2);
+        } catch (Throwable $e) {
+            Log::error($e);
+            throw $e;
+        }
 
         return redirect()
             ->route('admin.owners.index')
