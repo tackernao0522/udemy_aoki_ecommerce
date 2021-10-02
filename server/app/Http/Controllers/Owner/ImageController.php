12bbe56e482257fa1990535cc267a1dcb4e29a15
takaki <cheap_trick_magic@yahoo.co.jp>
2021-10-02 17:18:38 +0900

    イメージモデルのリレーションの設定及びプロダクトイメージ一覧ページの作成

--- server/app/Http/Controllers/Owner/ImageController.php
+++ server/app/Http/Controllers/Owner/ImageController.php
@@ -4,19 +4,38 @@ namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\UploadImageRequest;
 use App\Models\Image;
 use App\Models\Owner;
+use App\Services\ImageService;
 
 class ImageController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    public function __construct()
+    {
+        $this->middleware('auth:owners');
+
+        $this->middleware(function ($request, $next) {
+            $id = $request->route()->parameter('image'); //shopのid取得
+            if (!is_null($id)) { // null判定
+                $imagesOwnerId = Image::findOrFail($id)->owner->id;
+                $imageId = (int)$imagesOwnerId; // キャスト 文字列→数値に型変換 $ownerId = Auth::id();
+                if ($imageId !== Auth::id()) { // 同じでなかったら
+                    abort(404); // 404画面表示 php artisan vendor:publish --tag=laravel-errors (カスタム)
+                }
+            }
+            return $next($request);
+        });
+    }
+
     public function index()
     {
-        //
+        $images = Image::where('owner_id', Auth::id())
+            ->orderBy('updated_at', 'DESC')
+            ->paginate(20);
+
+        return view('owner.images.index', compact('images'));
     }
 
     /**
