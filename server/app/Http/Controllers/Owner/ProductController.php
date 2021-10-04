29a8b6e37ca7695f5a52509934be8615a9634b01
takaki <cheap_trick_magic@yahoo.co.jp>
2021-10-04 18:22:52 +0900

    商品管理ページのN+1問題の解消(Eager Load)

--- server/app/Http/Controllers/Owner/ProductController.php
+++ server/app/Http/Controllers/Owner/ProductController.php
@@ -4,17 +4,45 @@ namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Product;
+use App\Models\Image;
+use App\Models\Owner;
+use App\Models\SecondaryCategory;
 
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    public function __construct()
+    {
+        $this->middleware('auth:owners');
+
+        $this->middleware(function ($request, $next) {
+            $id = $request->route()->parameter('product'); //shopのid取得
+            if (!is_null($id)) { // null判定
+                $productsOwnerId = Product::findOrFail($id)->shop->owner->id;
+                $productId = (int)$productsOwnerId; // キャスト 文字列→数値に型変換 $ownerId = Auth::id();
+                if ($productId !== Auth::id()) { // 同じでなかったら
+                    abort(404); // 404画面表示 php artisan vendor:publish --tag=laravel-errors (カスタム)
+                }
+            }
+            return $next($request);
+        });
+    }
+
     public function index()
     {
-        //
+        // $products = Owner::findOrFail(Auth::id())->shop->products;
+        $ownerInfo = Owner::with('shop.products.imageFirst')
+            ->where('id', Auth::id())->get(); // Eager Load
+        // dd($ownerInfo);
+        // foreach($ownerInfo as $owner) {
+        //     // dd($owner->shop->products);
+        //     foreach($owner->shop->products as $product) {
+        //         dd($product->imageFirst->filename);
+        //     }
+        // }
+
+        return view('owner.products.index', compact('ownerInfo'));
     }
 
     /**
