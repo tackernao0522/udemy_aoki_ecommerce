c05f0149aa3c9e2117b6b1dfb09912c337fb8192
takaki <cheap_trick_magic@yahoo.co.jp>
2021-10-13 17:24:26 +0900

    Cartページの作成

--- server/app/Http/Controllers/User/CartController.php
+++ server/app/Http/Controllers/User/CartController.php
@@ -6,27 +6,39 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Cart;
+use App\Models\User;
 
 class CartController extends Controller
 {
+    public function index()
+    {
+        $user = User::findOrFail(Auth::id());
+        $products = $user->products; // 多対多のリレーション
+        $totalPrice = 0;
+
+        foreach ($products as $product) {
+            $totalPrice += $product->price * $product->pivot->quantity;
+        }
+
+        return view('user.cart', compact('products', 'totalPrice'));
+    }
+
     public function add(Request $request)
     {
-        $itemInCart = Cart::where('user_id', Auth::id())
-            ->where('product_id', $request->product_id)->first(); // カートに商品があるかどうか確認
+        $itemInCart = Cart::where('product_id', $request->product_id)
+            ->where('user_id', Auth::id())->first();
 
         if ($itemInCart) {
-            $itemInCart->quantity += $request->quantity; // あれば数量を追加
+            $itemInCart->quantity += $request->quantity;
             $itemInCart->save();
         } else {
             Cart::create([
                 'user_id' => Auth::id(),
                 'product_id' => $request->product_id,
-                'quantity' => $request->quantity,
+                'quantity' => $request->quantity
             ]);
-
-            dd('テスト');
-
-            return redirect()->route('user.cart.index');
         }
+
+        return redirect()->route('user.cart.index');
     }
 }
