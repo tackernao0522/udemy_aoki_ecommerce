9da022f04a881424d62d7716b96f5fc98506a91f
takaki <cheap_trick_magic@yahoo.co.jp>
2021-10-16 17:17:02 +0900

    pagination 一部修正

--- server/app/Models/Product.php
+++ server/app/Models/Product.php
@@ -111,17 +111,17 @@ class Product extends Model
         if ($sortOrder === null || $sortOrder === \Constant::SORT_ORDER['recommend']) {
             return $query->orderBy('sort_order', 'asc');
         }
-        if ($sortOrder === null || $sortOrder === \Constant::SORT_ORDER['higherPrice']) {
-            return $query->orderBy('sort_order', 'asc');
+        if ($sortOrder === \Constant::SORT_ORDER['higherPrice']) {
+            return $query->orderBy('price', 'desc');
         }
-        if ($sortOrder === null || $sortOrder === \Constant::SORT_ORDER['lowerPrice']) {
-            return $query->orderBy('sort_order', 'asc');
+        if ($sortOrder === \Constant::SORT_ORDER['lowerPrice']) {
+            return $query->orderBy('price', 'asc');
         }
-        if ($sortOrder === null || $sortOrder === \Constant::SORT_ORDER['later']) {
-            return $query->orderBy('sort_order', 'asc');
+        if ($sortOrder === \Constant::SORT_ORDER['later']) {
+            return $query->orderBy('products.created_at', 'desc');
         }
-        if ($sortOrder === null || $sortOrder === \Constant::SORT_ORDER['older']) {
-            return $query->orderBy('sort_order', 'asc');
+        if ($sortOrder === \Constant::SORT_ORDER['older']) {
+            return $query->orderBy('products.created_at', 'asc');
         }
     }
 }
